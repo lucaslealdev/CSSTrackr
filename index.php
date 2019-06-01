4a4f72ce13cc43bf1595f8dba8b2db0b48785e7e
Lucas Leal <lealzitooo@gmail.com>
2019-06-01 20:37:02 -0300

    update branch

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+ini_set('session.save_path',realpath('sessions'));
+ini_set('session.gc_probability', "1");
 ini_set('session.cookie_lifetime', strval(3600*24*7));
 ini_set('session.gc_maxlifetime', strval(3600*24*7));
 @session_set_cookie_params(strval(3600*24*7),"/"); //seven days
@@ -19,7 +21,19 @@ require('functions.php');
 /*get real user IP*/
 $_SERVER['REMOTE_ADDR'] = getRequestIP();
 
-if (isset($_GET) && !empty($_GET)){
+if(isset($_SERVER['HTTP_USER_AGENT']) && in_array(strtolower($_SERVER['HTTP_USER_AGENT']),$banned_agents)){
+  exit;
+}else if (in_array($_SERVER['REMOTE_ADDR'],$banned_ips)){
+  exit;
+} else {
+  foreach($banned_ips as $ip) {
+    if($ip==substr($_SERVER['REMOTE_ADDR'], 0,strlen($ip))){
+      exit;
+    }
+  }
+}
+
+if (isset($_GET) && !empty($_GET) && isset($_GET['action']) && isset($_GET['value'])){
 	if (!isset($_SESSION[S]['id'])){
 		/* new session */
 		if (!isset($_SESSION[S])) $_SESSION[S] = array();
